69cf740beb6f7bd3d3bde417a0c353e3ee9f5dc4
sabina Akter <aktersabina122@gmail.com>
2022-12-03 18:49:53 -0500

    put some lines

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 def my_function():
   print("Hello from Jenkins")
   print(".................................")
+  print(".................................")
+  print(".................................")
+  print("Continuous Testing Using Jenkins")
+   
 
 my_function()
+
